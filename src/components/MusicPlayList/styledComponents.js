3bdeb541e6f5e41c6d463dd8ee151f9f0208caeb
Eralingappa <nxtwave@ip-10-2-0-127.ap-south-1.compute.internal>
2024-06-12 17:27:23 +0000

    all test cases are fail

---
+++ src/components/MusicPlayList/styledComponents.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components'
+
+const BgContainer = styled.div`
+background-color:#152850;
+min-height:100vh;
+
+`
+export const ArtistBg = styled.div`
+background-image:url('https://assets.ccbp.in/frontend/react-js/music-playlist/music-playlist-Edsheeran-bg.png');
+height:450px;
+background-size:cver;
+
+`
+export const ArtistName = styled.h1`
+font-family:'Roboto';
+color:#ffffff;
+font-size:42px;
+
+`
+export const ArtistInfo = styled.p`
+font-family:'Roboto';
+color:#ffffff;
+font-size:32px;
+
+`
+export const PlayListContainer = styled.div`
+
+display:flex;
+align-items:center;
+`
+
+export const PlayListHeading = styled.h1`
+font-family:'Roboto';
+color:#ffffff;
+font-size:42px;
+`
+export const SearchInput=styled.input `
+height:30px;
+width:18%;
+color:#cbd5e1;
+border:1px solid #ffffff;
+
+`
+export const MusicList =styled.ul`
+padding:0px;
+margin-right:50px;
+margin-left:50px;
+
+
+`
+export const NoSongsText =styled.p`
+font-family:'Roboto';
+color:#ffffff;
+font-size:32px;
+
+`
